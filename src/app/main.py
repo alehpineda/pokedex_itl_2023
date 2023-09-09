4d5ef035ca8b4c4b27eb0d8b5136db615763e050
alehpineda <ale.hpineda@gmail.com>
2023-09-08 19:23:44 -0500

    new endpoint, requisitos

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,8 @@
+import requests
+
 from fastapi import FastAPI
+from typing import Dict
+from fastapi import HTTPException
 
 
 app = FastAPI()
@@ -12,3 +16,29 @@ async def root():
 @app.get("/healthcheck")
 async def healthscheck():
     return {"message": "Pokedex Up"}
+
+
+def pokedex_call_by_id(id: int) -> Dict:
+    try:
+        url = f"https://pokeapi.co/api/v2/pokemon/{id}"
+        response = requests.get(
+            url=url,
+            timeout=30,
+        )
+        response.raise_for_status()
+        return response.json()  # response.json is a python dict
+    except requests.exceptions.HTTPError as err:
+        raise HTTPException(status_code=response.status_code, detail=f"{err}")
+    except requests.exceptions.SSLError as err:
+        raise HTTPException(status_code=response.status_code, detail=f"{err}")
+    except requests.exceptions.Timeout as err:
+        raise HTTPException(status_code=response.status_code, detail=f"{err}")
+    except requests.exceptions.RequestException as err:
+        raise HTTPException(status_code=response.status_code, detail=f"{err}")
+    except Exception as err:
+        raise HTTPException(status_code=response.status_code, detail=f"{err}")
+
+
+@app.get("/pokedex/{id}")
+async def pokedex_by_id(id: int) -> Dict:
+    return pokedex_call_by_id(id=id)
